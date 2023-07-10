1920be2cd893433fdaa27404e5b63beaf33f280b
Porostik <porostik2@yandex.ru>
2023-07-10 09:57:14 +0300

    commit 1

--- index.ts
+++ index.ts
@@ -1,17 +1,5 @@
-import { getOctokit } from "@actions/github";
-
-const token = "123";
-
-const octokit = getOctokit(token);
-
-const main = async () => {
-  const data = await octokit.rest.pulls.listCommits({
-    owner: "Porostik",
-    repo: "test-octokit",
-    pull_number: 1,
-  });
-
-  console.log(data);
+const main = () => {
+  console.log(1);
 };
 
 main();
